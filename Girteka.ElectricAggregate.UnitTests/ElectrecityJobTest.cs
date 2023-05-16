837499d3fbfeb2c08da225a95ff190f8364c13c0
kogo <saba.koguashvili1999@mail.ru>
2023-05-16 21:57:57 +0400

    add new test

--- Girteka.ElectricAggregate.UnitTests/ElectrecityJobTest.cs
+++ Girteka.ElectricAggregate.UnitTests/ElectrecityJobTest.cs
@@ -1,15 +1,68 @@
+using System.Net;
+using Girteka.ElectricAggregate.Integrations;
+using Moq;
+using Moq.Protected;
+
 namespace Girteka.ElectricAggregate.UnitTests;
 
 public class ElectrecityJobTest
 {
     private HttpClient _httpClient;
+    private CSVFileFromHTTTP _csvFileFromHTTTP;
+
 
     [SetUp]
     public void Setup()
     {
         _httpClient = new HttpClient();
+        _csvFileFromHTTTP = new CSVFileFromHTTTP(_httpClient);
     }
 
+    [Test]
+    public void CSVFileFromHTTTPTest()
+    {
+        // Arrange
+        var localPath = "/Users/sabakoghuashvili/Desktop/Temp/";
+        var path = "https://data.gov.lt/dataset/1975/download/10766/2022-05.csv";
+
+        var response = new HttpResponseMessage(HttpStatusCode.OK);
+        var contentStream = new MemoryStream();
+        response.Content = new StreamContent(contentStream);
+
+        var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+        mockHttpMessageHandler
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(response);
+
+        var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+        var csvFileFromHTTTP = new CSVFileFromHTTTP(httpClient);
+
+        // Act
+        var result = csvFileFromHTTTP.Do(localPath, path);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsInstanceOf<MemoryStream>(result);
+    }
+    
+    [Test]
+    public void CSVFileFromLocalDiskTest()
+    {
+        // Arrange
+        var fileName = "2022-04.csv";
+        var path = "/Users/sabakoghuashvili/Desktop/Temp/";
+
+        var csvFileFromLocalDisk = new CSVFileFromLocalDisk();
+
+        // Act
+        var result = csvFileFromLocalDisk.Do(fileName, path);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsInstanceOf<FileStream>(result);
+    }
 
     [Test]
     public async Task CanDownloadCsvFiles()
@@ -32,28 +85,7 @@ public class ElectrecityJobTest
             Assert.IsNotEmpty(csvFileContent);
         }
     }
-
-    [Test]
-    public async Task CanReadCsvFiles()
-    {
-        FileStream result = null;
-        var localFiles = new[]
-        {
-            "/Users/sabakoghuashvili/Desktop/Temp/2022-05.csv",
-            "/Users/sabakoghuashvili/Desktop/Temp/2022-04.csv"
-        };
-
-        foreach (var path in localFiles)
-        {
-            string fileName = path.Split('/').Last();
-
-            result = File.Open(path, FileMode.Open);
-            
-            Assert.IsTrue(result.Length > 0);
-            result.Close();
-        }
-    }
-
+    
     [TearDown]
     public void Teardown()
     {
